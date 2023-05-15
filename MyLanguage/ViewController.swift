8504213e7752f6c8e99220ce81f120aae6aa1926
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-05-15 22:42:55 +0500

    Initial Commit

---
+++ MyLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLanguage
+//
+//  Created by Артем Павлов on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
